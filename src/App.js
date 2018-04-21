aa983e3d8e8db49faebca162cb5f10ef0538f229
Lemmah <lemayiannakolah@gmail.com>
2018-04-21 09:56:47 +0300

    [Feature] Add App Layout with react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Layout from './Layout.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <Layout>
+        </Layout>
       </div>
     );
   }
